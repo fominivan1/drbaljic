0edf907e77ff5c03d033ead8d2e9e802875aed32
foma94 <fominivan94@gmail.com>
2023-12-21 20:07:31 -0800

    Added new content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import PatientList from './components/PatientList';
-import AppointmentScheduler from './components/AppointmentScheduler';
-import PatientDetails from './components/PatientDetails';
-import Appointments from './components/Appointments';
+// import AppointmentScheduler from './components/AppointmentScheduler';
+// import Appointments from './components/Appointments';
 import Home from './components/Home';
+import About from './components/About'
+import './App.css';
 
 const AppContainer = styled.div`
   max-width: 800px;
@@ -15,58 +15,48 @@ const AppContainer = styled.div`
 `;
 
 const Navigation = styled.nav`
-  background-color: #333;
+  text-align: center;
   padding: 10px;
   color: white;
 
   a {
-    color: white;
+    color: black;
     text-decoration: none;
     margin-right: 15px;
   }
 `;
 
 const App = () => {
-  const [patients, setPatients] = useState([
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Smith' },
-    // Add more patient data as needed
-  ]);
 
-  const [appointments, setAppointments] = useState([
-    { id: 1, patientName: 'John Doe', date: '2023-01-01' },
-    { id: 2, patientName: 'Jane Smith', date: '2023-02-01' },
-    // Add more appointment data as needed
-  ]);
+  // const [appointments, setAppointments] = useState([
+  //   { id: 1, patientName: 'John Doe', date: '2023-01-01' },
+  //   { id: 2, patientName: 'Jane Smith', date: '2023-02-01' },
+  //   // Add more appointment data as needed
+  // ]);
 
-  const scheduleAppointment = ({ patientName, appointmentDate }) => {
-    const newAppointment = {
-      id: appointments.length + 1,
-      patientName,
-      date: appointmentDate,
-    };
+  // const scheduleAppointment = ({ patientName, appointmentDate }) => {
+  //   const newAppointment = {
+  //     id: appointments.length + 1,
+  //     patientName,
+  //     date: appointmentDate,
+  //   };
 
-    setAppointments([...appointments, newAppointment]);
+  //   setAppointments([...appointments, newAppointment]);
 
-    // You can add logic to send this appointment data to a server for storage
-    // This is a simplified example without server integration
-  };
+  //   // You can add logic to send this appointment data to a server for storage
+  //   // This is a simplified example without server integration
+  // };
 
   return (
     <Router>
       <AppContainer>
         <Navigation>
           <Link to="/">Home</Link>
-          <Link to="/patients">Patients</Link>
-          <Link to="/schedule">Schedule Appointment</Link>
-          <Link to="/appointments">Appointments</Link>
+          <Link to="/About">About</Link>
         </Navigation>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/patients" element={<PatientList patients={patients} />} />
-          <Route path="/schedule" element={<AppointmentScheduler scheduleAppointment={scheduleAppointment} />} />
-          <Route path="/appointments" element={<Appointments appointments={appointments} />} />
-          <Route path="/patient/:id" element={<PatientDetails patients={patients} />} />
+          <Route path="/About" element={<About />} />
         </Routes>
       </AppContainer>
     </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
-// src/components/Home.js
 import React from 'react';
 import styled from 'styled-components';
 
+
 const WelcomeContainer = styled.div`
-  background-color: #3498db;
-  color: #fff;
+  text-align: center;
+  font-size: 30px;
   padding: 20px;
   border-radius: 5px;
   margin-bottom: 20px;
@@ -16,18 +16,27 @@ const Description = styled.p`
   color: #333;
 `;
 
+const OfficeInfo = styled.p`
+  font-size: 18px;
+  line-height: 1.5;
+  color: #333;
+`;
+
 const Home = () => (
-  <div>
+    <div>
     <WelcomeContainer>
-      <h1>Welcome to the Doctor Baljic's Clinic App</h1>
-      <p>Your Trusted Partner in Healthcare</p>
+      <h1>Welcome to Dr. Baljic Family Care</h1>
+      <p>Quality Care You Can Trust</p>
     </WelcomeContainer>
     <Description>
       Our clinic is dedicated to providing high-quality healthcare services to our patients. With a team of
       experienced and compassionate doctors, we are committed to promoting the well-being of our community.
       Explore our patient management system, schedule appointments, and stay connected with your healthcare journey.
     </Description>
-  </div>
+    <OfficeInfo>
+      <p>{/* Your office info content goes here */}</p>
+    </OfficeInfo>
+    </div>
 );
 
 export default Home;
