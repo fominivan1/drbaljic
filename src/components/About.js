cd218a45548560852cca2dd385956473772b5747
foma94 <fominivan94@gmail.com>
2023-12-21 22:56:54 -0800

    added other test

--- src/components/About.js
+++ src/components/About.js
@@ -17,9 +17,6 @@ const StyledImage = styled.img`
 
 const About = () => (
   <div>
-<<<<<<< HEAD
-    <h1>hi</h1>
-=======
     <PictureContainer>
       <h1>Meet the Physician!</h1>
     <StyledImage src="images/crystal.jpg" alt="Description of the photo" />
@@ -30,7 +27,6 @@ const About = () => (
     <p>Christel Baljic, MD graduated in 2004 from the University of Illinois at Chicago and completed her physician training in Sacramento, CA at the Mercy Methodist Family Practice Residency Program. After ten years of experience at Med7 Urgent Care Centers (now Carbon Health), Christel opened her own micropractice in Rio Linda, CA.</p>
     </div>
     </PictureContainer>
->>>>>>> 9522def (added stuff)
   </div>
 
   
